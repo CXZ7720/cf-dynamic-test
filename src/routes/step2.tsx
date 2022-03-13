3a9131e8d1d2be2f9dc3798721274dd9241ebcaa
Aaron <aaron@daangn.com>
2022-03-14 04:12:38 +0900

    change

--- src/routes/step2.tsx
+++ src/routes/step2.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Step2 = () => {
     return (
         <div style={{backgroundColor: "yellow"}}>
-            <h1>Step 2</h1>
+            <h1>Step 2 - Changed!</h1>
             <Link to="/step3">Move to Step 3</Link>
         </div>
     )
